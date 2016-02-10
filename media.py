65f0aaa19bc2f9cefc7e8fc7f84d3f99e5581bb2
Adam <spam@adam.com>
2016-02-10 16:50:24 +1100

    clean up

--- media.py
+++ media.py
@@ -1,9 +1,10 @@
 # Class for storing Movie information
 class Movie():
     """ A Movie object with common attributes trailer, poster, etc"""
-    def __init__(self, title, poster, trailer, rating, genre): #constructor
-        self.trailer_youtube_url=trailer
-        self.title=title
-        self.poster_image_url=poster
-        self.genre=genre #lets extend with some other attributes
-        self.rating=rating #rating for the movie 
+
+    def __init__(self, title, poster, trailer, rating, genre):  # constructor
+        self.trailer_youtube_url = trailer
+        self.title = title
+        self.poster_image_url = poster
+        self.genre = genre  # lets extend with some other attributes
+        self.rating = rating  # rating for the movie
